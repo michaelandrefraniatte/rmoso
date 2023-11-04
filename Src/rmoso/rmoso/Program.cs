7daae1c586ba4614397e990eef57f7d22be4a458
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-11-04 20:23:58 +0100

    Initial commit

---
+++ Src/rmoso/rmoso/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace rmoso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tEnter a word to search in files");
+            string searched = Console.ReadLine();
+            string rootPath = System.Windows.Forms.Application.StartupPath;
+            string[] fileNames = Directory.GetFiles(rootPath);
+            foreach (string fileName in fileNames)
+            {
+                string txt = File.ReadAllText(fileName);
+                if (txt.Contains(searched))
+                {
+                    Console.WriteLine(fileName);
+                }
+            }
+            string[] dirs = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+            foreach (string dir in dirs)
+            {
+                string[] files = Directory.GetFiles(dir);
+                foreach (string file in files)
+                {
+                    string txt = File.ReadAllText(file);
+                    if (txt.Contains(searched))
+                    {
+                        Console.WriteLine(file);
+                    }
+                }
+            }
+            Console.WriteLine("done");
+            Console.ReadLine();
+        }
+    }
+}
